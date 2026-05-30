787d79ef701e3affaccbddf728e0c499052db514
Pablo Aragon <p.aragon.83114@gmail.com>
2026-05-30 02:12:20 +0200

    vim: add nvim-lspconfig plugin and fix bashls LSP config

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp-bash.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp-bash.lua
@@ -1,13 +1,8 @@
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      bashls = {
-        cmd = { "bash-language-server", "start" },
-        filetypes = { "bash", "sh" },
-        root_pattern = { ".bashrc", ".bash_profile", ".profile" },
-        single_file_support = true,
-      },
-    },
-  },
+vim.lsp.config.bashls = {
+  cmd = { "bash-language-server", "start" },
+  filetypes = { "bash", "sh" },
+  root_markers = { ".bashrc", ".bash_profile", ".profile" },
+  single_file_support = true,
 }
+
+vim.lsp.enable("bashls")
