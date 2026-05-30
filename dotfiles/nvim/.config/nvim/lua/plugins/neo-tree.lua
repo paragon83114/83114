5af1681682776acdf013dba3681b016caeec41c2
Pablo Aragon <p.aragon.83114@gmail.com>
2026-05-30 03:08:57 +0200

    vim: add neo-tree.nvim as alternative file explorer

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_diagnostics = true,
+      sources = { "filesystem", "buffers", "git_status" },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        width = 60,
+      },
+    })
+    vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle file explorer" })
+  end,
+}
