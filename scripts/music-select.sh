acc7eafe1b221bf03a92d27a5774400acf05c759
Pablo Aragon <p.aragon.83114@gmail.com>
2026-05-30 14:11:38 +0200

    Ollama eliminado como indicar de tmux

--- scripts/music-select.sh
+++ scripts/music-select.sh
@@ -2,4 +2,4 @@
 
 MUSIC_DIR="$HOME/storage/music"
 
-cd "$MUSIC_DIR" && ls | fzf --height=60% --border=rounded --prompt="Musica: " --reverse --no-multi --preview-window=right:40% --preview="killall mpv 2>/dev/null ; pwd && mpv --no-video --osd-level=0 "{}""
+cd "$MUSIC_DIR" && ls | fzf --height=100% --border=rounded --prompt="Musica: " --reverse --no-multi --preview-window=right:40% --preview="killall mpv 2>/dev/null ; pwd && mpv --no-video --osd-level=0 "{}""
