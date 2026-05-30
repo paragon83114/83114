616f0ef76bf7d5999929e9dbb2e3b139adbff9d2
Pablo Aragon <p.aragon.83114@gmail.com>
2026-05-30 02:32:40 +0200

    vim: update which-key config to register keybindings

--- dotfiles/nvim/.config/nvim/lua/plugins/which-key.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/which-key.lua
@@ -5,5 +5,12 @@ return {
   config = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
+
+    local wk = require("which-key")
+
+    wk.add({
+      { "<leader>e", group = "explorer" },
+      { "<leader>e", desc = "Toggle file explorer", mode = "n" },
+    })
   end,
 }
