e8ee51dc6489d78e6980abaae438c936250106b3
Pablo Aragon <p.aragon.83114@gmail.com>
2026-05-30 02:14:55 +0200

    vim: fix lsp-bash plugin spec

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp-bash.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp-bash.lua
@@ -1,8 +1,13 @@
-vim.lsp.config.bashls = {
-  cmd = { "bash-language-server", "start" },
-  filetypes = { "bash", "sh" },
-  root_markers = { ".bashrc", ".bash_profile", ".profile" },
-  single_file_support = true,
+return {
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  config = function()
+    vim.lsp.config.bashls = {
+      cmd = { "bash-language-server", "start" },
+      filetypes = { "bash", "sh" },
+      root_markers = { ".bashrc", ".bash_profile", ".profile" },
+      single_file_support = true,
+    }
+    vim.lsp.enable("bashls")
+  end,
 }
-
-vim.lsp.enable("bashls")
